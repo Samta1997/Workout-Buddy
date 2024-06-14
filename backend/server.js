eb2b992551e98fd8a377f5a38e2682af7b421d6b
Samta1997 <dharmendersinghsml@gmail.com>
2024-06-14 15:40:33 +0530

    server deployment try

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ app.use((req, res, next)=>{             //this will be executed for every reques
     console.log(req.path, req.method);
     next();
 })
+app.get('/',()=>{
+    console.log('Welcome to Workout buddy server')
+
+})
 app.use('/api/workout',workoutRoutes); //for setting workout routes
 app.use('/api/user',userRoutes);  //for setting user routes
 
